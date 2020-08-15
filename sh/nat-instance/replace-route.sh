fd264d02732dac3ed40ce68e8a0e1eb71028fceb
Chris <knightchris2001@yahoo.com>
2020-08-14 23:23:47 -0400

    Try as parameters

--- sh/nat-instance/replace-route.sh
+++ sh/nat-instance/replace-route.sh
@@ -3,4 +3,4 @@
 INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 REGION=`wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document | jq --raw-output '.region'`
 
-aws ec2 replace-route --region ${REGION} --route-table-id ${ROUTE_TABLE_ID} --destination-cidr-block "0.0.0.0/0" --instance-id ${INSTANCE_ID} || aws ec2 create-route --region ${REGION} --route-table-id ${ROUTE_TABLE_ID} --destination-cidr-block "0.0.0.0/0" --instance-id ${INSTANCE_ID}
+aws ec2 replace-route --region ${REGION} --route-table-id $1 --destination-cidr-block "0.0.0.0/0" --instance-id ${INSTANCE_ID} || aws ec2 create-route --region ${REGION} --route-table-id $1 --destination-cidr-block "0.0.0.0/0" --instance-id ${INSTANCE_ID}
