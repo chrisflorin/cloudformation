040d55a9881b65ebbf1786834c5358904878d943
chrisflorin <knightchris2001@yahoo.com>
2020-08-29 21:12:28 -0400

    Update Nat Instance to work for 2 availability zones

--- sh/common/code-deploy-amazon-linux-ami.sh
+++ sh/common/code-deploy-amazon-linux-ami.sh
@@ -5,7 +5,7 @@ REGION=`wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/doc
 yum install -y ruby wget
 
 cd /home/ec2-user
-wget https://aws-codedeploy-$REGION.s3.$REGION.amazonaws.com/latest/install
+wget https://aws-codedeploy-${REGION}.s3.${REGION}.amazonaws.com/latest/install
 
 chmod +x ./install
 ./install auto
